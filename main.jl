bbd3120fd65b714051063e5488c4aa108c515883
Vincent Su <phazedphantom@gmail.com>
2020-05-25 16:29:15 -0700

    Adding argparse

--- main.jl
+++ main.jl
@@ -4,23 +4,38 @@ include("./util.jl")
 include("./hamiltonian.jl")
 using .util
 using .hamiltonian
+using ArgParse
 
 
+s = ArgParseSettings()
+@add_arg_table! s begin
+    "--N_sites", "-N"
+    help = "Number of sites"
+    arg_type = Int
+    default = 8
+end
+
+args = parse_args(s)
+N = args["N_sites"]
+
+
+assert false
 
 sweeps = Sweeps(20)
 maxdim!(sweeps, 10,20,100,100,200, 500, 1000, 1000, 2000, 2000, 2000, 4000)
 cutoff!(sweeps, 1E-50)
 
-for N in 8:2:20
-    H_TFIM, sites = TFIM_Hamiltonian(N)
+H_TFIM, sites = TFIM_Hamiltonian(N)
 
-    e_0, psi_0 = ground_state(H_TFIM, sites, sweeps)
-    wfs = [psi_0]
-    e_1, psi_1 = excited_state(wfs, H_TFIM, sites, sweeps)
-    wfs = [psi_0 psi_1]
+e_0, psi_0 = ground_state(H_TFIM, sites, sweeps)
+wfs = [psi_0]
+e_1, psi_1 = excited_state(wfs, H_TFIM, sites, sweeps)
+wfs = [psi_0 psi_1]
 
-    s_rel = calculate_relative_entropies(psi_1, psi_0, N)
-    # println(s_rel)
+s_rel = calculate_relative_entropies(psi_1, psi_0, N)
+# println(s_rel)
 
-    @save "TFIM_$N.jld2" wfs s_rel
+fname = "TFIM_$N"
+save_MPS(wfs[1], fname, "wf1")
+save_MPS(wfs[2], fname, "wf2")
 end
