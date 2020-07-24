ab8ea4227f200832a2d22c7017cb27fdbafb3733
Vincent Su <vipasu@berkeley.edu>
2020-07-23 17:27:26 -0700

    Export Hamiltonian dictionary instead of individual functions.
    Avoids the need for having to call individual hamiltonian functions.
    Instead different hamiltonian constructions are wrapped in a dictionary and
    referenced by their model name.

--- first_excited_state.jl
+++ first_excited_state.jl
@@ -1,5 +1,4 @@
 using ITensors
-using JLD2, FileIO
 include("./util.jl")
 include("./hamiltonian.jl")
 using .util
@@ -10,32 +9,35 @@ using ArgParse
 s = ArgParseSettings()
 @add_arg_table! s begin
     "--N_sites", "-N"
-    help = "Number of sites"
-    arg_type = Int
-    default = 8
+      help = "Number of sites"
+      arg_type = Int
+      default = 8
+    "--model"
+      help = "Name of spin chain"
+      arg_type = String
+      default = "TFIM"
 end
 
 args = parse_args(s)
 N = args["N_sites"]
+model = args["model"]
 
-
-assert false
 
 sweeps = Sweeps(20)
 maxdim!(sweeps, 10,20,100,100,200, 500, 1000, 1000, 2000, 2000, 2000, 4000)
 cutoff!(sweeps, 1E-50)
 
-H_TFIM, sites = TFIM_Hamiltonian(N)
+H, sites = H_dict[model](N)
 
-e_0, psi_0 = ground_state(H_TFIM, sites, sweeps)
+e_0, psi_0 = ground_state(H, sites, sweeps)
 wfs = [psi_0]
-e_1, psi_1 = excited_state(wfs, H_TFIM, sites, sweeps)
+e_1, psi_1 = excited_state(wfs, H, sites, sweeps)
 wfs = [psi_0 psi_1]
+println("Success!")
+println(wfs)
 
-s_rel = calculate_relative_entropies(psi_1, psi_0, N)
-# println(s_rel)
 
-fname = "TFIM_$N"
+
+fname = "$model_$N"
 save_MPS(wfs[1], fname, "wf1")
 save_MPS(wfs[2], fname, "wf2")
-end
--- hamiltonian.jl
+++ hamiltonian.jl
@@ -1,7 +1,9 @@
 module hamiltonian
     using ITensors
     export TFIM_Hamiltonian, XXZ_Hamiltonian, ground_state, excited_state
-    export Heisenberg_1, WZW_2_2
+    export Heisenberg_Hamiltonian, WZW_2_2_Hamiltonian
+    export H_dict
+
 
     function TFIM_Hamiltonian(N,
                             Jz::Float64=-1.,
@@ -14,13 +16,13 @@ module hamiltonian
         # a Hamiltonian matrix, and convert
         # these terms to an MPO tensor network
         for j=1:N-1
-          add!(ampo, 4 * Jz, "Sz",j,"Sz",j+1)
+          add!(ampo, 4 * Jz, "Sz", j, "Sz", j+1)
         end
         if (pbc)
             add!(ampo, 4 * Jz, "Sz", N, "Sz", 1)
         end
         for j=1:N
-          add!(ampo, 2 * h, "Sx",j)
+          add!(ampo, 2 * h, "Sx", j)
         end
 
         H = MPO(ampo,sites)
@@ -28,7 +30,7 @@ module hamiltonian
     end
 
 
-    function WZW_2_2(N)
+    function WZW_2_2_Hamiltonian(N)
         sites = siteinds("S=1",N)
         ampo = AutoMPO()
         for j=1:N-1
@@ -75,19 +77,19 @@ module hamiltonian
     end
 
 
-    function Heisenberg_1(N)
+    function Heisenberg_Hamiltonian(N)
         # Input operator terms which define a Hamiltonian
         sites = siteinds("S=1",N)
         ampo = AutoMPO()
         for j=1:N-1
-          add!(ampo,1 ,"S+",j,"S-",j+1)
-          add!(ampo,1 ,"S-",j,"S+",j+1)
-          add!(ampo,2 , "Sz",j, "Sz",j+1)
+          add!(ampo, 1, "S+", j, "S-", j+1)
+          add!(ampo, 1, "S-", j, "S+", j+1)
+          add!(ampo, 2, "Sz", j, "Sz", j+1)
         end
         # PBC
-        add!(ampo,1,"S+",N,"S-",1)
-        add!(ampo,1,"S-",N,"S+",1)
-        add!(ampo,2, "Sz",N, "Sz",1)
+        add!(ampo, 1, "S+", N, "S-", 1)
+        add!(ampo, 1, "S-", N, "S+", 1)
+        add!(ampo, 2, "Sz", N, "Sz", 1)
 
         # Convert these terms to an MPO tensor network
 
@@ -96,20 +98,20 @@ module hamiltonian
     end
 
     function XXZ_Hamiltonian(N,
-                            J::Float64=1.,
-                            Delta::Float64=.5)
+                             J::Float64=1.,
+                             Delta::Float64=.5)
         # Input operator terms which define a Hamiltonian
         sites = siteinds("S=1/2",N)
         ampo = AutoMPO()
         for j=1:N-1
-          add!(ampo,4 * J/2.,"S+",j,"S-",j+1)
-          add!(ampo,4 * J/2.,"S-",j,"S+",j+1)
-          add!(ampo,4 * Delta, "Sz",j, "Sz",j+1)
+          add!(ampo, 4 * J/2., "S+", j, "S-", j+1)
+          add!(ampo, 4 * J/2., "S-", j, "S+", j+1)
+          add!(ampo, 4 * Delta, "Sz", j, "Sz", j+1)
         end
         # PBC
-        add!(ampo,4 * J/2.,"S+",N,"S-",1)
-        add!(ampo,4 * J/2.,"S-",N,"S+",1)
-        add!(ampo,4 * Delta, "Sz",N, "Sz",1)
+        add!(ampo, 4 * J/2., "S+", N, "S-", 1)
+        add!(ampo, 4 * J/2., "S-", N, "S+", 1)
+        add!(ampo, 4 * Delta, "Sz", N, "Sz", 1)
 
         # Convert these terms to an MPO tensor network
 
@@ -135,4 +137,9 @@ module hamiltonian
         energy, psi
     end
 
+    H_dict = Dict("TFIM" => TFIM_Hamiltonian,
+                  "XXZ" => XXZ_Hamiltonian,
+                  "Heisenberg" => Heisenberg_Hamiltonian,
+                  "WZW" => WZW_2_2_Hamiltonian
+                  )
 end
